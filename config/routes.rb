0cc9590e3f4785cf8b2a7100ac00d8fa252cb1fb
JaviL13 <javipaz.larrain@uc.cl>
2023-05-28 17:08:44 -0400

    intento de render

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'render/index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For more details on this file's DSL, see https://guides.rubyonrails.org/routing.html
+  root 'render#index'
 end
